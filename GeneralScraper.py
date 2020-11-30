41c0086217fef6efe02b6b983cd51ed769a084de
antonFredrikSvensson <antonfredriksvensson@gmail.com>
2020-11-30 23:46:27 +0100

    test with 10 pages working AB#255

--- GeneralScraper.py
+++ GeneralScraper.py
@@ -2,14 +2,35 @@ from bs4 import BeautifulSoup, Tag
 import urllib.request
 import datetime
 
-def Scrape(url):
-    sauce = urllib.request.urlopen(url).read()
-    soup = BeautifulSoup(sauce,'lxml')
-    #sint = ignore_exception(ValueError)(int) #TODO check what sint is used for
+def Fetch_pages(soup):
+    page_buttons = soup.findAll("a", {"class": "gZwUSm"})
+    no_of_page_buttons = len(page_buttons)
+    no_of_pages = int(page_buttons[no_of_page_buttons-1].text)
+    return no_of_pages
+    
+
+def Scrape_page(soup):
     articles = soup.findAll('article')
     article_list = Extract_articles(articles)
     articles_content = Extract_article_content(article_list)
-    print(articles_content[0]["location"])
+    return articles_content
+
+def Scrape(url):
+    sauce = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(sauce,'lxml')
+    no_of_pages = Fetch_pages(soup)
+    pages_content = []
+    no_of_pages = 10 ##TODO remove variable after tests
+    for page in range(no_of_pages):
+        if page != 0:
+            page_number = page + 2
+            postfix = "page=" + str(page_number)
+            newurl = url + postfix
+            sauce = urllib.request.urlopen(newurl).read()
+            soup = BeautifulSoup(sauce,'lxml')
+        articles_content = Scrape_page(soup)
+        pages_content.append(articles_content)
+    print(pages_content[2][0])
 
 def Extract_articles(articles):
     article_list =[]
@@ -69,7 +90,13 @@ def Extract_subject(subject_wrapper):
     return href_subject_id
 
 def Extract_price(sales_info):
-    price_text = sales_info.div.div.span.div.text
+    try:
+        price_text = sales_info.div.div.span.div.text
+    except:
+        price_text = ''
+        price = None
+        price_info = [price, price_text]
+        return price_info
     if price_text =='':
         price = None
     else:
